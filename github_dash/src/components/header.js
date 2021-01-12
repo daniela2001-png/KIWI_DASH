c67c346bb0e218bbe62d9bbca33c026d56453c49
daniela2001-png <dmbmora@outlook.com>
2021-01-11 21:30:37 -0500

    [Update] Adding props for pass to other components

--- github_dash/src/components/header.js
+++ github_dash/src/components/header.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import "../styles/header.css";
-import { Statistic, Icon, Image } from 'semantic-ui-react'
+import { Statistic, Icon } from 'semantic-ui-react'
 
 /*
-This part of code contains the infomration basic about of username
-like:
+This part of code contains the infomration basic about of username like:
+
 1) number of repos
 2) number of  users following
 3) number of users followers
@@ -16,24 +16,24 @@ Note: In this file I added somes styles inlines for somes containers
 
 */
 
-export default function HeaderDiv({ repos, following, followers, avatar, location, gists, type }) {
+export default function HeaderDiv({ gists, repos, following, followers, avatar, location, type }) {
     return (
         <>
-            <div style={{ backgroundColor: "#41484D" }} class="container p-3 my-3 border">
+            <div style={{ backgroundColor: "#41484D" }} className="container p-3 my-3 border">
                 <br></br>
-                <div class="row">
-                    <div class="col-sm-8">
+                <div className="row">
+                    <div className="col-sm-8">
                         <div className="container">
                             <Statistic.Group>
-                                <Statistic>
+                                <Statistic color='olive' inverted>
                                     <Statistic.Value>{repos}</Statistic.Value>
                                     <Statistic.Label>repositories</Statistic.Label>
                                 </Statistic>
-                                <Statistic>
+                                <Statistic color='teal' inverted>
                                     <Statistic.Value>{following}</Statistic.Value>
                                     <Statistic.Label>following</Statistic.Label>
                                 </Statistic>
-                                <Statistic>
+                                <Statistic color='blue' inverted>
                                     <Statistic.Value>{followers}</Statistic.Value>
                                     <Statistic.Label>followers</Statistic.Label>
                                 </Statistic>
@@ -42,11 +42,11 @@ export default function HeaderDiv({ repos, following, followers, avatar, locatio
                         <br></br>
                         <div style={{ paddingBottom: "100px" }} className="container">
                             <Statistic.Group>
-                                <Statistic>
-                                    <Statistic.Value>0</Statistic.Value>
+                                <Statistic color='brown' inverted>
+                                    <Statistic.Value>{gists}</Statistic.Value>
                                     <Statistic.Label>gists</Statistic.Label>
                                 </Statistic>
-                                <Statistic>
+                                <Statistic color='red' inverted>
                                     <Statistic.Value text>
                                         <Icon name='user' />
                                         <br />
@@ -54,7 +54,7 @@ export default function HeaderDiv({ repos, following, followers, avatar, locatio
                                     </Statistic.Value>
                                     <Statistic.Label>type</Statistic.Label>
                                 </Statistic>
-                                <Statistic>
+                                <Statistic inverted>
                                     <Statistic.Value>
                                         <Icon name='home' />
                                     </Statistic.Value>
@@ -63,8 +63,8 @@ export default function HeaderDiv({ repos, following, followers, avatar, locatio
                             </Statistic.Group>
                         </div>
                     </div>
-                    <div class="col-sm-4">
-                        <img src={avatar} class="ui medium circular image" />
+                    <div className="col-sm-4">
+                        <img alt="avatar-user-github" src={avatar} className="ui medium circular image" />
                     </div>
                 </div>
             </div>
